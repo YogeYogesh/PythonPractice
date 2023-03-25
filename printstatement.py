5264c9a3979b2b3b0505902f1f23ca8add512f7b
YogeYogesh <128778681+YogeYogesh@users.noreply.github.com>
2023-03-25 11:23:12 +0530

    Add files via upload

---
+++ printstatement.py
@@ -0,0 +1,9 @@
+print("\t\t\t\tSTUDENT INFORMATION")
+print("--------------------------------------------------------------------------------")
+print("\tNAME\t\t: YOGESH KUMAR M")
+print("\tAGE\t\t: 28")
+print("\tEMAIL\t\t: janayogesh1994@gmail.com")
+print("\tADDRESS\t\t: 48, Nethaji Street")
+print("\tMOBILE NO\t: 8122294010")
+print("--------------------------------------------------------------------------------")
+
---
+++ variables.py
@@ -0,0 +1,17 @@
+print("\t\t\t\tSTUDENT INFORMATION")
+print("--------------------------------------------------------------------------------")
+e="Yogesh Kumar"
+print("\tNAME\t\t: YOGESH KUMAR M")
+a=28
+b=8122294010
+c="janayogesh1994@gmail.com"
+d="48, Nethaji Street"
+print("\tAGE\t\t:",a)
+print("\tEMAIL\t\t:",c)
+print("\tADDRESS\t\t:",d)
+print("\tMOBILE NO\t:",b)
+print("--------------------------------------------------------------------------------")
+
+print(" ")
+print("\tName and Age:",a,b)
+
