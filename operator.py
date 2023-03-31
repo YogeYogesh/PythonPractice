e95c85299bb4a11119733c3da358ae50c16b4c14
YogeYogesh <128778681+YogeYogesh@users.noreply.github.com>
2023-03-31 09:20:36 +0530

    Operators

---
+++ operator.py
@@ -0,0 +1,71 @@
+print("addition")
+num1=int(input("entet the number "))
+num2=int(input("enter the number "))
+addition=num1+num2
+print("Additon=",addition)
+print(" ")
+print("subtraction")
+num1=int(input("entet the number "))
+num2=int(input("enter the number "))
+subtraction=num1-num2
+print("Subtraction=",subtraction)
+print(" ")
+print("multiplication")
+num1=int(input("entet the number "))
+num2=int(input("enter the number "))
+multiplication=num1*num2
+print("Multiplication=",multiplication)
+print(" ")
+print("division")
+num1=int(input("entet the number "))
+num2=int(input("enter the number "))
+division=num1/num2
+print("Division=",division)
+#Area of Circle
+print(" ")
+print("Area of Circle")
+radius=int(input("enter the radius of circle"))
+pi=2
+area=pi*radius*radius
+print("area of circle",area)
+#Area of Rectangle
+print(" ")
+print("Area of Rectangle")
+length=int(input("enter the length number"))
+breadth=int(input("enter the breadth number"))
+answer=length*breadth
+print("Area of Rectangle",answer)
+
+#Area of Square
+print(" ")
+print("Area of Square")
+area=int(input("enter the area of square value"))
+square=area*area
+print("area of square",square)
+
+#Area of Triangle
+print(" ")
+print("Area of Triangle")
+breadth=int(input("enter the breadth number"))
+height=int(input("enter the heigth number"))
+triangle=1/2*breadth*height
+print("Area of Triangle",triangle)
+
+#Simple Interest
+print("Simple Interest")
+p=int(input("enter the principle number"))
+n=int(input("enter the number of years"))
+r=int(input("enter the rate"))
+pnr=p*n*r/100
+print("Simple Interest",pnr)
+
+
+
+
+
+
+
+
+
+
+
