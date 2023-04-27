913c46d23bbbaeb69498c44dcebe7be97eb3ac29
YogeYogesh <128778681+YogeYogesh@users.noreply.github.com>
2023-04-27 09:49:59 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,57 @@
+##hellow_name
+##
+##a=input("Enter the name")
+##b=input("Enter the name")
+##print(a+" "+b)
+##
+##make_abba
+##a=input("Enter the name")
+##b=input("Enter the name")
+##print(a+b+b+a)
+##
+##make_tags
+##tag=input("Enter the Tag")
+##name=input("Enter the name")
+##print(tag+name+tag)
+##
+##make_out_word
+##a="<<>>"
+##b="Yay"
+##print(a[0:2]+b+a[2:4])
+##
+##extra_end
+##a="Hello"
+##print(a[3:5]*3)
+##
+##First Two
+##a="Hello"
+##print(a[0:2]*3)
+##
+##First Half
+##a="WooHoo"
+##print(a[0:3])
+##
+##
+##without end
+##a="Hello"
+##print(a[-4:-1])
+##
+##Combo String
+##a="Hello"
+##b="hi"
+##print(b+a+b)
+##
+##non_Start
+##a="Hellow"
+##b="There"
+##print(a[1:5]+b[1:5])
+##
+#Left2
+a="Hello"
+print(a[2:5]+a[0:2])
+
+
+
+
+
+##
