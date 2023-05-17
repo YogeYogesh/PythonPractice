8b7859c61fe7c491c15170b38089e4b376d3c156
YogeYogesh <128778681+YogeYogesh@users.noreply.github.com>
2023-05-17 10:08:01 +0530

    Add files via upload

---
+++ funtion_even or odd number.py
@@ -0,0 +1,16 @@
+number=int(input("Enter the Number: "))
+def even_or_odd(number):
+    if number%2==0:
+        print("This is even number",number)
+    else:
+        print("This is odd number",number)
+even_or_odd(number)
+
+
+def positive_or_negative(number):
+    if number>0:
+        print("This is Positive number",number)
+    else:
+        print("This is Negative nubmer",number)
+positive_or_negative(number)
+
